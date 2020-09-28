71a29c091048f762e18de2f682cfb2c3895d431d
caklipakli <simonfalvi.ildiko@ext.t-systems.hu>
2020-09-28 12:30:44 +0200

    Add reasource

---
+++ db/migrate/20200928102316_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+      t.string :category
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end
